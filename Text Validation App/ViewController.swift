0bd53e21da61d913aa30251f702ea7d8899f7985
Jacob Davis <jacobdofficial@gmail.com>
2023-10-02 13:18:00 -0600

    Initial Commit

---
+++ Text Validation App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Text Validation App
+//
+//  Created by Jacob Davis on 10/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
