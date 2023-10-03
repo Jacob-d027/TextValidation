436953ac3826d653ce420c0290c28b147ba904e9
Jacob Davis <jacobdofficial@gmail.com>
2023-10-03 14:45:28 -0600

    Initial Commit of completed project.

---
+++ Text Validation App/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  Text Validation App
+//
+//  Created by Jacob Davis on 10/3/23.
+//
+
+import Foundation
+
+struct User {
+    var username: String
+    var password: String
+}
--- Text Validation App/ViewController.swift
+++ Text Validation App/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var errorMessageLabel: UILabel!
+    
+//    var users: [User] = [User(username: "somecatlover@somecompanyname.com", password: "B00mer&Rac00n!"),
+//    User(username: "someweirdo@somecompanyname.com", password: "Il0vet0r3ad!")]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func submitButtonPressed(_ sender: Any) {
+        validatePassword()
+    }
+    
+    func validatePassword() {
+        guard let password = passwordTextField.text else {return}
 
+        if password.count < 8 {
+            errorMessageLabel.textColor = UIColor.red
+            errorMessageLabel.text = "The password is not long enough."
+        } else if !password.contains(where: {",.!@#$%^&*<>?+=-".contains($0) }) {
+            errorMessageLabel.textColor = UIColor.red
+            errorMessageLabel.text = "Password must contain a special character."
+        } else {
+            errorMessageLabel.textColor = UIColor.green
+            errorMessageLabel.text = "Login Successful!"
+        }
+    }
+    
+    
 }
 
